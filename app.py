4bc0a2316a96c369a04a131b4901902eecae5264
JanTektSan <yuniverse9999@hotmail.com>
2023-08-02 11:57:11 +0900

    using page object pattern for app and test

--- app.py
+++ app.py
@@ -4,22 +4,8 @@ from selenium import webdriver
 browser = webdriver.Firefox()
 browser.implicitly_wait(5)
 
-browser.get('https://www.instagram.com/')
-
-login_link = browser.find_element_by_xpath("//a[text()='Log in']")
-login_link.click()
-
-sleep(2)
-
-username_input = browser.find_element_by_css_selector("input[name='username']")
-password_input = browser.find_element_by_css_selector("input[name='password']")
-
-username_input.send_keys("<your username>")
-password_input.send_keys("<your password>")
-
-login_button = browser.find_element_by_xpath("//button[@type='submit']")
-login_button.click()
-
-sleep(5)
+home_page = HomePage(browser)
+login_page = home_page.go_to_login_page()
+login_page.login("<your username>", "<your password>")
 
 browser.close()
--- test.py
+++ test.py
@@ -1,11 +1,7 @@
 def test_login_page(browser):
-	browser.get('https://www.instagram.com/accounts/login')
-	username_input = browser.find_element_by_css_selector("input[name='username']")
-	password_input = browser.find_element_by_css_selector("input[name='password']")
-	username_input.send_keys("<your username>")
-	password_input.send_keys("<your password>")
-	login_button = browser.find_element_by_xpath("//button[@type='submit']")
-	login_button.click()
+	home_page = HomePage(browser)
+	login_page = home_page.go_to_login_page()
+	login_page.login("<your username>", "<your password>")
 
 	errors = browser.find_element_by_css_selector('#error_message')
 	assert len(errors) == 0
